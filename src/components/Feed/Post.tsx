b1b61dd552e0df84308da7ff1e8dea71ec2df313
Peter Lisy <pelisy@microsoft.com>
2016-10-26 01:24:53 +0200

    Drawing the image in a canvas and an emoticon on top of it

--- src/components/Feed/Post.tsx
+++ src/components/Feed/Post.tsx
@@ -9,13 +9,75 @@ export interface PostData {
 }
 
 class Post extends React.Component<PostData, void> {
+
+    private image: HTMLImageElement;
+    private canvas: HTMLCanvasElement;
+    private canvasContext: CanvasRenderingContext2D | null;
+
+    componentWillMount(): void {
+        this.image = document.createElement('img');
+        this.image.src = this.props.imageURL;
+    }
+
+    componentDidMount() {
+        let scaleFactor = 0;
+
+        if (this.image.width >= this.image.height) {
+            scaleFactor = this.canvas.width / this.image.width;
+        } else {
+            scaleFactor = this.canvas.height / this.image.height;
+        }
+
+        if (!this.canvasContext) {
+            return;
+        }
+
+        this.canvasContext.scale(scaleFactor, scaleFactor);
+
+        this.canvasContext.drawImage(this.image, 0, 0);
+
+        const smily = this.createImageComponent('https://static-asm.secure.skypeassets.com/pes/v1/emoticons/happy/views/default_160');
+
+        smily.onload = () => {
+            this.props.emotions.forEach(emotion => {
+                this.canvasContext && this.canvasContext.drawImage(
+                    smily,
+                    emotion.faceRectangle.left,
+                    emotion.faceRectangle.top,
+                    emotion.faceRectangle.width,
+                    emotion.faceRectangle.height,
+                )
+            });
+        }
+
+    }
+
+    createImageComponent(url: string): HTMLImageElement {
+        const img = document.createElement('img');
+        img.src = url;
+        return img;
+    }
+
+    componentWillUpdate(nextProps: PostData, nextState: void, nextContext: any): void {
+        this.image = document.createElement('img');
+        this.image.src = nextProps.imageURL;
+    }
+
+    private setCanvasRef = (canvas: HTMLCanvasElement) => {
+        this.canvas = canvas;
+
+        if (canvas) {
+            this.canvasContext = canvas.getContext('2d');
+        }
+    };
+
     render() {
         console.log(this.props.emotions);
 
         return (
             <Card>
                 <div className="Post">
-                    <img src={this.props.imageURL} alt=""/>
+                    <canvas width={640} height={640} ref={this.setCanvasRef} />
                 </div>
             </Card>
         );
